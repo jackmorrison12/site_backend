9227c43a3fd4f9995492a3af3b9393dfbf2131d1
jackmorrison <jsm1317@ic.ac.uk>
2021-01-06 17:39:27 +0000

    Removes unused code

--- apiManager.js
+++ apiManager.js
@@ -6,14 +6,14 @@ var ApiLastUpdatedHandler = require("./apiLastUpdatedHandler.js");
 module.exports = class APIManager {
   constructor() {}
 
-  // DONE Needs to keep track of when each API was last fetched.
-  // DONE This can be kept in the database using a different model
+  // Needs to keep track of when each API was last fetched.
+  // This can be kept in the database using a different model
   // Has external update method. This is the only thing called, and it does all the rest internally.
   // Update is called, it checks the database to see which APIs it needs to update (could have forced override)
   // Then it calls the method for each API, which itself updates the database
 
-  // Store name of API, min time since last update before it should be updated again (in ms) and it's API Handler class
 
+  // Store name of API, min time since last update before it should be updated again (in ms) and it's API Handler class
   static async update() {
     const apis = {
       lastfm: { update_frequency: 180000, handler: LastFMHandler },

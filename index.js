b5da74978792198bc303393f4462e713ccfa3215
jackmorrison <jsm1317@ic.ac.uk>
2021-01-10 02:03:47 +0000

    Fixes type error of imported classes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var ApiFragmentHandler = require("./apiFragmentHandler.js");
-var APIManager = require("./apiManager.js");
+const ApiFragmentHandler = require("./apiFragmentHandler.js");
+const APIManager = require("./apiManager.js");
 // Import express
 let express = require("express");
 // Import Body parser
